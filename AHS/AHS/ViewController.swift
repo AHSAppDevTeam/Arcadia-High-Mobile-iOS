036a10937d41604f7ec8df4af2ff033970e3d113
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-03-13 21:40:44 -0800

    Create initial xcode project

---
+++ AHS/AHS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AHS
+//
+//  Created by Richard Wei on 3/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
