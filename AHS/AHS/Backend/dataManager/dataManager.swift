36949eae40a17e2cf875c7b2be617b2fd453d858
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-03-17 22:35:29 -0700

    Reinstall missing pods + add pods from old project

---
+++ AHS/AHS/Backend/dataManager/dataManager.swift
@@ -0,0 +1,14 @@
+//
+//  dataManager.swift
+//  AHS
+//
+//  Created by Richard Wei on 3/17/21.
+//
+
+import Foundation
+import Firebase
+import Firebase
+
+class dataManager{
+    //static internal var firRef : DatabaseReference!;
+}
