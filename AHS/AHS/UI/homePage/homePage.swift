a6d962c8dad2f6d0c381234cbb904c5e99df1253
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-03-28 20:42:03 -0700

    Add remaining views + finish autolayout constraints

--- AHS/AHS/UI/homePage/homePage.swift
+++ AHS/AHS/UI/homePage/homePage.swift
@@ -23,8 +23,16 @@ class homePageViewController : mainPageViewController{
     //
     
     internal var mainScrollView : UIScrollView = UIScrollView();
-    internal var topCatagoryPickerView : UIButton = UIButton();
-    internal var topCatagoryPickerHeightAnchor : NSLayoutConstraint = NSLayoutConstraint();
+    
+    // main scrollview views
+    internal var topCatagoryPickerView : UIView = UIView();
+    internal var topCatagoryPickerViewHeightAnchor : NSLayoutConstraint = NSLayoutConstraint();
+    
+    internal var featuredCategoryView : UIView = UIView();
+    internal var featuredCategoryViewHeightAnchor : NSLayoutConstraint = NSLayoutConstraint();
+    
+    internal var contentView : UIView = UIView();
+    internal var contentViewHeightAnchor : NSLayoutConstraint = NSLayoutConstraint();
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -51,7 +59,7 @@ class homePageViewController : mainPageViewController{
     
     private func setupTopCatagoryPickerView(){
         
-        //topCatagoryPickerView = UIView(frame: CGRect(x: 0, y: 0, width: mainScrollView.frame.width, height: mainScrollView.frame.height / 10));
+        //top category
         mainScrollView.addSubview(topCatagoryPickerView);
         topCatagoryPickerView.backgroundColor = .systemRed;
         
@@ -60,18 +68,37 @@ class homePageViewController : mainPageViewController{
         topCatagoryPickerView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor).isActive = true;
         topCatagoryPickerView.topAnchor.constraint(equalTo: mainScrollView.topAnchor).isActive = true;
         topCatagoryPickerView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor).isActive = true;
-        topCatagoryPickerView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor).isActive = true;
+        //topCatagoryPickerView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor).isActive = true;
         
-        topCatagoryPickerHeightAnchor = topCatagoryPickerView.heightAnchor.constraint(equalToConstant: 100);
-        topCatagoryPickerHeightAnchor.isActive = true;
+        topCatagoryPickerViewHeightAnchor = topCatagoryPickerView.heightAnchor.constraint(equalToConstant: 100);
+        topCatagoryPickerViewHeightAnchor.isActive = true;
         
-        topCatagoryPickerView.addTarget(self, action: #selector(self.test), for: .touchUpInside);
+        //
         
+        mainScrollView.addSubview(featuredCategoryView);
+        featuredCategoryView.backgroundColor = .systemGreen;
         
-    }
-    
-    @objc func test(_ sender: UIButton){
-        topCatagoryPickerHeightAnchor.constant += 100;
+        featuredCategoryView.translatesAutoresizingMaskIntoConstraints = false;
+        featuredCategoryView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor).isActive = true;
+        featuredCategoryView.topAnchor.constraint(equalTo: topCatagoryPickerView.bottomAnchor).isActive = true;
+        featuredCategoryView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor).isActive = true;
+        
+        featuredCategoryViewHeightAnchor = featuredCategoryView.heightAnchor.constraint(equalToConstant: 100);
+        featuredCategoryViewHeightAnchor.isActive = true;
+        
+        //
+        
+        mainScrollView.addSubview(contentView);
+        contentView.backgroundColor = .systemOrange;
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false;
+        contentView.leadingAnchor.constraint(equalTo: mainScrollView.leadingAnchor).isActive = true;
+        contentView.topAnchor.constraint(equalTo: featuredCategoryView.bottomAnchor).isActive = true;
+        contentView.widthAnchor.constraint(equalTo: mainScrollView.widthAnchor).isActive = true;
+        contentView.bottomAnchor.constraint(equalTo: mainScrollView.bottomAnchor).isActive = true;
+        
+        contentViewHeightAnchor = contentView.heightAnchor.constraint(equalToConstant: 500);
+        contentViewHeightAnchor.isActive = true;
     }
     
     
