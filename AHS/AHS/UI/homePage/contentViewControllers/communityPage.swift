caabaecb3fbee69d0af500d0fdc70fc17aa42a71
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-05-23 00:20:56 -0700

    Modified each category view to fit to title height

--- AHS/AHS/UI/homePage/contentViewControllers/communityPage.swift
+++ AHS/AHS/UI/homePage/contentViewControllers/communityPage.swift
@@ -43,7 +43,7 @@ class communityPageController : homeContentPageViewController{
     
     internal func renderCategory(_ categorydata: categoryData){
         
-        let categoryViewFrame = CGRect(x: homePageHorizontalPadding, y: nextContentY, width: AppUtility.getCurrentScreenSize().width - 2*homePageHorizontalPadding, height: AppUtility.getCurrentScreenSize().width);
+        let categoryViewFrame = CGRect(x: homePageHorizontalPadding, y: nextContentY, width: AppUtility.getCurrentScreenSize().width - 2*homePageHorizontalPadding, height: AppUtility.getCurrentScreenSize().width * 1.2);
         let categoryView = CategoryButton(frame: categoryViewFrame);
         categoryView.tag = 1;
         
@@ -71,21 +71,9 @@ class communityPageController : homeContentPageViewController{
         
         // content inside content view ----
         
-        let categoryImagesViewFrame = CGRect(x: 0, y: 0, width: categoryContentView.frame.width, height: categoryView.frame.height * 0.6);
-        let categoryImagesView = UIView(frame: categoryImagesViewFrame);
-        
-        categoryImagesView.layer.cornerRadius = categoryContentView.frame.height / 20;
-        categoryImagesView.clipsToBounds = true;
-        categoryImagesView.isUserInteractionEnabled = false;
-        
-        categoryImagesView.backgroundColor = .systemRed;
-        
-        categoryContentView.addSubview(categoryImagesView);
-        
-        //
-        
-        let categoryFindViewHeight = categoryContentView.frame.height * 0.08;
-        let categoryFindViewFrame = CGRect(x: categoryContentViewHorizontalPadding, y: categoryContentView.frame.height - categoryFindViewHeight - categoryContentViewVerticalPadding, width: (categoryContentView.frame.width * 0.45) - 2*categoryContentViewHorizontalPadding, height: categoryFindViewHeight);
+        let categoryFindViewWidth = (categoryContentView.frame.width * 0.45) - 2*categoryContentViewHorizontalPadding;
+        let categoryFindViewHeight = categoryFindViewWidth * 0.2;
+        let categoryFindViewFrame = CGRect(x: categoryContentViewHorizontalPadding, y: categoryContentView.frame.height - categoryFindViewHeight - categoryContentViewVerticalPadding, width: categoryFindViewWidth, height: categoryFindViewHeight);
         let categoryFindView = UIView(frame: categoryFindViewFrame);
         
         
@@ -103,7 +91,6 @@ class communityPageController : homeContentPageViewController{
         
         categoryFindView.addSubview(categoryFindImageView);
         
-        //
         
         let categoryFindLabelHorizontalPadding = categoryFindView.frame.width / 15;
         let categoryFindLabelFrame = CGRect(x: categoryFindLabelHorizontalPadding, y: 0, width: categoryFindView.frame.width - 2*categoryFindLabelHorizontalPadding - categoryFindImageViewWidth - categoryFindImageViewHorizontalPadding, height: categoryFindView.frame.height);
@@ -128,17 +115,35 @@ class communityPageController : homeContentPageViewController{
         
         //
         
-        let categoryDescriptionLabelFrame = CGRect(x: categoryContentViewHorizontalPadding, y: categoryImagesView.frame.height + categoryContentViewVerticalPadding, width: categoryContentView.frame.width - 2*categoryContentViewHorizontalPadding, height: categoryContentView.frame.height - categoryImagesView.frame.height - 3*categoryContentViewVerticalPadding - categoryFindView.frame.height);
+        let categoryDescriptionLabelText = categorydata.blurb;
+        let categoryDescriptionLabelWidth = categoryContentView.frame.width - 2*categoryContentViewHorizontalPadding;
+        let categoryDescriptionLabelFont = UIFont(name: SFProDisplay_Semibold, size: UIScreen.main.scale * 8)!;
+        let categoryDescriptionLabelHeight = categoryDescriptionLabelText.height(withConstrainedWidth: categoryDescriptionLabelWidth, font: categoryDescriptionLabelFont)
+        
+        let categoryDescriptionLabelFrame = CGRect(x: categoryContentViewHorizontalPadding, y: categoryContentView.frame.height - categoryFindView.frame.height - categoryDescriptionLabelHeight - 2*categoryContentViewVerticalPadding, width: categoryDescriptionLabelWidth, height: categoryDescriptionLabelHeight);
         let categoryDescriptionLabel = UILabel(frame: categoryDescriptionLabelFrame);
         
         categoryDescriptionLabel.textAlignment = .left;
         categoryDescriptionLabel.textColor = InverseBackgroundColor;
-        categoryDescriptionLabel.text = categorydata.blurb;
-        categoryDescriptionLabel.numberOfLines = 2;
-        categoryDescriptionLabel.font = UIFont(name: SFProDisplay_Semibold, size: categoryDescriptionLabel.frame.height * 0.4);
+        categoryDescriptionLabel.text = categoryDescriptionLabelText;
+        categoryDescriptionLabel.numberOfLines = 0;
+        categoryDescriptionLabel.font = categoryDescriptionLabelFont;
         categoryDescriptionLabel.isUserInteractionEnabled = false;
         
         categoryContentView.addSubview(categoryDescriptionLabel);
+        
+        //
+        
+        let categoryImagesViewFrame = CGRect(x: 0, y: 0, width: categoryContentView.frame.width, height: categoryContentView.frame.height - categoryFindView.frame.height - categoryDescriptionLabel.frame.height - 3*categoryContentViewVerticalPadding);
+        let categoryImagesView = UIView(frame: categoryImagesViewFrame);
+        
+        categoryImagesView.layer.cornerRadius = categoryContentView.frame.height / 20;
+        categoryImagesView.clipsToBounds = true;
+        categoryImagesView.isUserInteractionEnabled = false;
+        
+        categoryImagesView.backgroundColor = .systemRed;
+        
+        categoryContentView.addSubview(categoryImagesView);
         
         
         // ---
