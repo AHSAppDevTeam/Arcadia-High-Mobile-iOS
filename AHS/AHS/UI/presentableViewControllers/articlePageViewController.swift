6defaa4205a0c646e5c21113d79bb7bebc6387f6
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-05-26 23:03:05 -0700

    Set up layout for article page

--- AHS/AHS/UI/presentableViewControllers/articlePageViewController.swift
+++ AHS/AHS/UI/presentableViewControllers/articlePageViewController.swift
@@ -12,15 +12,64 @@ import UIKit
 class articlePageViewController : presentableViewController{
     
     public var articleID : String = "";
+    internal var nextContentY : CGFloat = 0;
+    
+    let scrollView : UIScrollView = UIScrollView();
+    let refreshControl : UIRefreshControl = UIRefreshControl();
     
     override func viewDidLoad() {
         super.viewDidLoad();
-        
-        self.view.backgroundColor = .systemBlue;
-        
-        setupPanGesture();
+    
+        renderUI();
+        loadArticleData();
     }
     
+    internal func renderUI(){
+        
+        self.view.backgroundColor = BackgroundColor;
+        setupPanGesture();
+        
+        //
+        
+        let topBarViewFrame = CGRect(x: 0, y: AppUtility.safeAreaInset.top, width: self.view.frame.width, height: self.view.frame.width * 0.15);
+        let topBarView = UIView(frame: topBarViewFrame);
+        
+        topBarView.backgroundColor = .systemPink;
+        
+        self.view.addSubview(topBarView);
+        
+        //
+        
+        scrollView.frame = CGRect(x: 0, y: topBarView.frame.maxY, width: self.view.frame.width, height: self.view.frame.height - topBarView.frame.maxY);
+        self.view.addSubview(scrollView);
+        
+        //scrollView.backgroundColor = InverseBackgroundColor;
+        scrollView.alwaysBounceVertical = true;
+        
+        refreshControl.addTarget(self, action: #selector(self.handleRefresh), for: .valueChanged)
+        scrollView.addSubview(refreshControl);
+        
+    }
     
+    @objc internal func handleRefresh(){
+        
+        for subview in scrollView.subviews{
+            if (subview.tag == 1){
+                subview.removeFromSuperview();
+            }
+        }
+        
+        loadArticleData();
+    }
+    
+    internal func loadArticleData(){
+        dataManager.getFullArticleData(articleID, completion: { (data) in
+            self.renderArticle(data);
+        });
+    }
+    
+    internal func renderArticle(_ articleData: fullArticleData){
+        
+    }
     
 }
