cb8301cf106b7bd1b3598efb4147b742d50d3ef2
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2021-06-19 00:57:00 -0700

    Set up bulletin page

--- AHS/AHS/UI/bulletinPage.swift
+++ AHS/AHS/UI/bulletinPage.swift
@@ -23,6 +23,17 @@ class bulletinPageViewController : mainPageViewController{
     
     //
     
+    internal let mainScrollView : UIScrollView = UIScrollView();
+
+    internal let categoryScrollView : UIScrollView = UIScrollView();
+    
+    internal let comingUpLabel : UILabel = UILabel();
+    internal let comingUpContentView : UIView = UIView();
+    
+    internal let bulletinContentView : UIView = UIView();
+    
+    //
+    
     override func viewDidLoad() {
         super.viewDidLoad();
     }
@@ -32,10 +43,19 @@ class bulletinPageViewController : mainPageViewController{
         
         if (!self.hasBeenSetup){
             
-            self.view.backgroundColor = .blue;
+            mainScrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height);
+            self.view.addSubview(mainScrollView);
+            
+            self.renderLayout();
             
             self.hasBeenSetup = true;
         }
+        
+    }
+    
+    internal func renderLayout(){
+        
+        
         
     }
 }
