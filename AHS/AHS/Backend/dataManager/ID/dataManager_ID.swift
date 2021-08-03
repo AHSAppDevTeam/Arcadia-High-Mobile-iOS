0281e13370d5835e8d9a65f01e4633eecd5f1ab2
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-08-03 00:56:40 -0700

    Prevent domain phishing of email addresses
    Closes #19

--- AHS/AHS/Backend/dataManager/ID/dataManager_ID.swift
+++ AHS/AHS/Backend/dataManager/ID/dataManager_ID.swift
@@ -14,7 +14,7 @@ import AVFoundation
 extension dataManager{
     
     static public func isValidStudentEmail(_ email: String) -> Bool{
-        let pattern = "^[0-9]{5}@students.ausd.net$";
+        let pattern = #"^[0-9]{5}@students\.ausd\.net$"#;
         let regex = try! NSRegularExpression(pattern: pattern);
         return regex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.utf16.count)) != nil;
     }
