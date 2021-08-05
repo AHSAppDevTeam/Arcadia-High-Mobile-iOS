c4392a9b1dbe60259e5f33530a5a38f11585ff07
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2021-08-04 17:00:00 -0700

    Shorten regex email checking

--- AHS/AHS/Backend/dataManager/ID/dataManager_ID.swift
+++ AHS/AHS/Backend/dataManager/ID/dataManager_ID.swift
@@ -14,9 +14,8 @@ import AVFoundation
 extension dataManager{
     
     static public func isValidStudentEmail(_ email: String) -> Bool{
-        let originalRegex = try! NSRegularExpression(pattern: #"^[0-9]{5}@students\.ausd\.net$"#);
-        let secondaryRegex = try! NSRegularExpression(pattern: #"^[0-9]{5}@ausd\.net$"#);
-        return originalRegex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.utf16.count)) != nil || secondaryRegex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.utf16.count)) != nil;
+        let regex = try! NSRegularExpression(pattern: #"^[0-9]{5}@(students\.)?ausd\.net$"#);
+        return regex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.utf16.count)) != nil;
     }
     
     static public func getIDBarcode(_ s: String) -> UIImage{
