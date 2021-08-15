901d853ac621f8e30d9a421897054c77ea67095f
Richard Wei <48197502+richardwei6@users.noreply.github.com>
2021-08-15 16:34:15 -0700

    Reimplement firebase sign in

--- AHS/AHS/Backend/dataManager/ID/dataManager_SSO.swift
+++ AHS/AHS/Backend/dataManager/ID/dataManager_SSO.swift
@@ -18,7 +18,7 @@ extension dataManager{
     
     static public func recoverPreviousSignInSession(){
         GIDSignIn.sharedInstance.restorePreviousSignIn(callback: { (user, error) in
-            //signInFirebase(user, error: error, completion: { _ in });
+            signInFirebase(user, error: error, completion: { _ in });
         });
     }
     
@@ -29,16 +29,16 @@ extension dataManager{
         let config = GIDConfiguration(clientID: clientID)
         
         GIDSignIn.sharedInstance.signIn(with: config, presenting: parentVC) { (user, error) in
-            /*signInFirebase(user, error: error, completion: { (err) in
+            signInFirebase(user, error: error, completion: { (err) in
                 
                 completion(err);
                 
-            });*/
-            completion(error);
+            });
+            //completion(error);
         }
     }
     
-    /*static private func signInFirebase(_ user: GIDGoogleUser?, error: Error?, completion: @escaping (Error?) -> Void){
+    static private func signInFirebase(_ user: GIDGoogleUser?, error: Error?, completion: @escaping (Error?) -> Void){
         if let err = error {
             completion(err);
             return;
@@ -56,17 +56,17 @@ extension dataManager{
             completion(error);
             
         });
-    }*/
+    }
     
     static public func signOutUser(){
-        /*do{
+        do{
             try Auth.auth().signOut();
             GIDSignIn.sharedInstance.signOut();
         }
         catch{
             print("Failed to sign out - \(error.localizedDescription)");
-        }*/
-        GIDSignIn.sharedInstance.signOut();
+        }
+        //GIDSignIn.sharedInstance.signOut();
     }
     
 }
